361164a6871a489d35bf44ecf2fa0c6d33987f86
arbuztw <arbuz.tw@gmail.com>
2015-05-26 22:06:57 +0800

    temporary server

--- server.py
+++ server.py
@@ -1,20 +1,33 @@
 #-*- encoding: utf-8 -*-
+import sys
 import tornado.ioloop
 import tornado.web
 import util
 from multiprocessing import Pool
+sys.path.append("./liblinear-1.96/python")
+import liblinearutil as liblr
+
+m = liblr.load_model("./10.model")
 
 class MainHandler(tornado.web.RequestHandler):
    def get(self, tag):
+      p = Pool(10)
       if tag == "":
          return
-      medias = util.search_by_tag(tag, 5)
+      media_gen = util.search_by_tag(tag, 5)
+      for _ in range(0, 10):
+         medias = media_gen.next()
 
-      p = Pool(5)
-      p.map(util.features, medias)
-      for media in medias:
-         self.write("<img src=%s />" % media["images"]["low_resolution"]["url"])
-         self.flush()
+         fs = p.map(util.features, medias)
+
+         p_label, _, _ = liblr.predict([], fs, m)
+         for x in zip(p_label, fs):
+            print x
+         for (label, (f, media)) in zip(p_label, zip(fs, medias)):
+            self.write("<img src=%s />%s %s %d<br>" % (media["images"]["low_resolution"]["url"], media["caption"]["text"], str(f), label))
+            self.flush()
+      p.close()
+      p.join()
 
 application = tornado.web.Application([
    (r"/(.*)", MainHandler),
--- util.py
+++ util.py
@@ -4,33 +4,29 @@ import requests
 import sys
 import time
 import config
-from instagram.client import InstagramAPI
 
 token = config.token
 social_media = [u"whatsapp", u"facebook", u"wechat", u"line"]
 keywords = [u"$", u"購", u"禮", u"優惠", u"郵寄", u"價", u"面交", u"shop", u"sell"]
 days = 5
-api = InstagramAPI(access_token=token)
 
 def search_by_tag(tag, count=20):
    url = "https://api.instagram.com/v1/tags/"+tag+"/media/recent?access_token="+token+"&count="+str(count)
 
-   #while True:
-   req = requests.get(url)
-   jdata = json.loads(req.text)
-   return jdata["data"]
-   #url = jdata["pagination"]["next_url"]
+   while True:
+      req = requests.get(url)
+      jdata = json.loads(req.text)
+      yield jdata["data"]
+      url = jdata["pagination"]["next_url"]
 
 def get_info(id):
-    '''
+    
     req = requests.get("https://api.instagram.com/v1/users/"+id+"/media/recent?min_timestamp="+str(int(time.time())-86400*days)+"&access_token="+token)
     
     if req.status_code == 200:
         last = len(json.loads(req.text)["data"])
     else:
         return "", 0, 0
-    '''
-    last = 0
     
     req = requests.get("https://api.instagram.com/v1/users/"+id+"?access_token="+token)
     
@@ -44,11 +40,10 @@ def get_info(id):
 
 def matching(content):
     
-    content.replace(" ", "")
-    content.replace("-", "")
+    content = content.replace(" ", "").replace("-", "").lower()
 
     wcount = sum(map(lambda s: content.count(s) , keywords))
-    socount = sum(map(lambda s: s in content, social_media))
+    socount = sum(map(lambda s: content.count(s), social_media))
     li = [wcount, socount]
     
     mx = 0
@@ -71,17 +66,15 @@ def features(post):
     comment = post["comments"]["count"]
     content = post["caption"]["text"]
   
-    #print(follower)
     out.append(tag_amount)
     out.append(like)
     out.append(comment)
     out.append(follower)
     out.append(post_last)
-    #out += [0,0]
 
     out += matching(author_info)
     out += matching(content)
 
-    return dict(enumerate(out))
+    return out
 
 
